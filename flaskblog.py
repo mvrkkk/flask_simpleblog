029e5aa376c63994e9099ebca0793e61ab564e86
mvrkkk <mrk.stakan@gmail.com>
2020-02-12 13:11:28 +0200

    added main app

---
+++ flaskblog.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import sys
+import pandas as pd
+
+sys.path.append('../')
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+    <h1>Hello world!</h1>
+    <a href="/about"> Go about </a> 
+    """
+
+@app.route('/about')
+def about():
+    return "<h1>About page!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug = True)
