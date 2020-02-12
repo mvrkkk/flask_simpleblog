782f8406bdb9599e2e908659af064e1b3213f789
mvrkkk <mrk.stakan@gmail.com>
2020-02-12 15:53:43 +0200

    added forms.py classes, added empty templates login/register; added arcticle template

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for
 from datetime import datetime
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a2da0e1db3516e7d0d18800d449a48c8'
 
 posts_data = [
     {
@@ -28,6 +30,17 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
